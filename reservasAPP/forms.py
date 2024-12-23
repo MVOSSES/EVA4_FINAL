2f5aca84d9336b1309bec564a151f2b78eda1174
MVOSSES <mvossesmeza@gmail.com>
2024-12-23 00:10:16 -0300

    Modificado el archivo forms.py

--- reservasAPP/forms.py
+++ reservasAPP/forms.py
@@ -3,16 +3,19 @@ from django.core.exceptions import ValidationError
 from reservasAPP.models import Reservas, Estado
 
 #editar reservas
+def validar_cantidad_minima(value):
+    if value <= 0:
+        raise ValidationError('La cantidad de personas debe ser mayor a 0.')
+def validar_cantidad_maxima(value):
+    if value >= 16:
+        raise ValidationError('La cantidad máxima es 15.')
 class CrearReservas(forms.Form):
     nombre= forms.CharField()
     telefono= forms.CharField()
     fecha_reserva= forms.DateField()
     hora= forms.TimeField()
     cantidad_personas= forms.IntegerField(
-        validators=[
-            lambda value: value > 0 or ValidationError('La cantidad de personas debe ser mayor a 0'),
-            lambda value: value < 16 or ValidationError('La cantidad máxima es 16.')
-        ]
+        validators=[validar_cantidad_minima, validar_cantidad_maxima]
     )
     estado= forms.ModelChoiceField(queryset=Estado.objects.all())
     observacion= forms.CharField(required=False)
@@ -27,14 +30,6 @@ class CrearReservas(forms.Form):
 
 
 #agregar reservas
-# Definición de validadores personalizados
-def validar_cantidad_minima(value):
-    if value <= 0:
-        raise ValidationError('La cantidad de personas debe ser mayor a 0.')
-
-def validar_cantidad_maxima(value):
-    if value >= 16:
-        raise ValidationError('La cantidad máxima es 15.')
 
 class CrearReservas(forms.ModelForm):
     nombre = forms.CharField()
